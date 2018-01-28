d476da6228dc44c31fb378f3bac8676083cc55da
hccmbd123 <35863297+hccmbd123@users.noreply.github.com>
2018-01-28 06:25:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,12 +27,43 @@ def form():
 		urllib._urlopener = AppURLopener()
 		f = urllib.urlopen("http://datawrapper.dwcdn.net/aL51p/3/")
 		myfile = f.read()
-		print myfile
-		return f.read()
+		opioids = "Hydrocodone (Vicodin and Norco) 5 mg tablet\tCodeine (Tylenol #3) 30 mg tablets\tTramadol 50 mg tablets\tOxycodne 5 mg tablets"
+		newfile= "\nLaparoscopic Cholecystectomy\t15\t15\t15\t10\nLaparoscopic Appendectomy\t15\t15\t15\t10\nInguinal\/Femoral Hernia Repair (open\/laparoscopic)\t15\t15\t15\t10\nOpen Incisional Hernia Repair\t40\t40\t40\t25\nLaparoscopic Colectomy\t35\t35\t35\t25\nOpen Colectomy\t40\t40\t40\t25\nHysterectomy vaginal \t20\t20\t20\t15\nHyseretomy Laparoscopic & Robotic\t30\t30\t30\t20\nHysterectomy Abdominal \t40\t40\t40\t25\nWide Local Excision \u00b1 Sentinel Lymph Node Biopsy\t30\t30\t30\t20\nSimple Mastectomy \u00b1 Sentinel Lymph Node Biopsy\t30\t30\t30\t20\nLumpectomy \u00b1 Sentinel Lymph Node Biopsy\t15\t15\t15\t10\nBreast Biopsy or Sentinel Lymph Node Biopsy\t15\t15\t15\t10"
+		mydata = newfile.split("\n")
+		opioidString = opioids.split("\t")
+		for line in mydata:
+			cell = line.split("\t")
+			if cell[0].strip().upper() == singleRecord.procedure.strip().upper():
+				for med in opioidString:	
+					if med.strip().upper() == singleRecord.medication.strip().upper():
+						return  "<br></br><br></br><br></br><center>Here are your results...<br></br><br></br>If you are no longer in pain, but still have extra medication,<br></br> you can visit " + "http://disposemymeds.org/medicine-disposal-locator/" + " to find the closest facility to safely dispense of them.</center><br></br><br></br><br></br> This is your procedure and the amount of pills you SHOULD be prescribed after surgery:    " + cell[0] + " , " + med + " , " + cell[1] + " tablets."
 
-		    # print(link.get('href'))
+						# pro = cell[0]  
+						# medi = med
+						# s = """ <center> Here are your results...</center
+						# Procedure = {0},
+						# Medication = {1}
+						# """
+						
 
-		return "http://disposemymeds.org/medicine-disposal-locator/"
+						# return """ 
+						# 	<body>
+						# 	<br></br>
+						# 	<br></br>
+						# 	<br></br>
+						# 	<br></br>
+						# 	</body>
+						# 	<center> Here are your results...</center
+
+						# """
+						# cell[0] + med
+
+						
+						
+						#return render_template('datastyles.html' % data )
+
+		#create another file and create button to click here
+		#return "http://disposemymeds.org/medicine-disposal-locator/"
 		# html_file = open("graphs.html")
 		# html_response = html_file.read()
 
@@ -41,6 +72,7 @@ def form():
 		print(e)
 		return "oops"
 	return ""
+
 
 @app.route("/")
 def hello():
@@ -60,3 +92,5 @@ except:
 	records = []
 
 app.run()
+if __name__ == '__app__':
+   app.run(host='127.0.0.1', port=8080, debug=True)
