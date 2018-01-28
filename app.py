3862596924d322304e7000ee0f804e72797905b9
hccmbd123 <35863297+hccmbd123@users.noreply.github.com>
2018-01-28 02:23:00 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import pickle
+from pickleHack import Record
+from flask import Flask, request
+from bs4 import BeautifulSoup
+import urllib
+import subprocess
+from subprocess import call
+
+
+app = Flask(__name__)
+
+
+#This makes an endpoint that the form will go to
+@app.route("/form", methods=["POST"])
+def form():
+	print (request.form)
+	try:
+		values = request.form
+		print(values)
+		singleRecord = Record(values['age'], values['procedure'], values['zipcode'],values['gender'], values['race'], values['height'], values['weight'], values['medication'],values['dosage'],values['doses'])
+		print(singleRecord)
+		records.append(singleRecord)
+		pickle.dump(records,open('records.pickle','wb'))
+
+
+		class AppURLopener(urllib.FancyURLopener):version = "Mozilla/5.0"
+		urllib._urlopener = AppURLopener()
+		f = urllib.urlopen("http://datawrapper.dwcdn.net/aL51p/3/")
+		myfile = f.read()
+		print myfile
+		return f.read()
+
+		    # print(link.get('href'))
+
+		return "http://disposemymeds.org/medicine-disposal-locator/"
+		# html_file = open("graphs.html")
+		# html_response = html_file.read()
+
+		# return html_response
+	except Exception as e:
+		print(e)
+		return "oops"
+	return ""
+
+@app.route("/")
+def hello():
+	html_file = open("structure.html")
+	html_response = html_file.read()
+	return html_response
+
+
+# this is tge goodbye thing
+@app.route("/goodbye")
+def goodbye():
+	return "goodbye"
+
+try:
+	records = pickle.load(open('records.pickle','rb'))
+except:
+	records = []
+
+app.run()
---
+++ pickleHack.py
@@ -0,0 +1,19 @@
+# IDK how to do this shit
+
+class Record:
+	def __init__(self, age, procedure, zipcode, gender, race, height, weight, medication, dosage, doses):
+
+
+			self.age = age
+			self.procedure = procedure
+			self.zipcode = zipcode
+			self.gender = gender
+			self.race = race
+			self.height = height
+			self.weight = weight
+			self.medication = medication
+			self.dosage = dosage
+			self.doses = doses
+			
+
+
